faab6f216d3a9fea858de312907aae14530d4208
Artur Ptaszek <artur.ptaszek@gmail.com>
2022-04-10 13:52:09 +0200

    feat: Adjust arguments

--- src/lib/args.rs
+++ src/lib/args.rs
@@ -1,21 +1,36 @@
-use clap::{Parser, Subcommand};
+use clap::{ArgEnum, Parser, Subcommand};
 
 #[derive(Subcommand, Debug)]
-pub enum Flows {
-    AuthorizationCodeWithPKCE { client_id: String },
+pub enum Flow {
+    AuthorizationCodeWithPKCE,
     AuthorizationCode,
     Implicit,
     ClientCredentials,
+}
+
+#[derive(Debug, ArgEnum, Clone)]
+pub enum Token {
+    IdToken,
+    AccessToken,
 }
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
 pub struct Arguments {
     #[clap(subcommand)]
-    pub flow: Flows,
+    pub flow: Flow,
 
     #[clap(long)]
     pub issuer_url: String,
+
+    #[clap(long)]
+    pub client_id: String,
+
+    #[clap(long, default_value = "offline_access")]
+    pub scope: String,
+
+    #[clap(long, arg_enum, default_value_t = Token::AccessToken)]
+    pub token: Token,
 }
 
 pub struct Args;
