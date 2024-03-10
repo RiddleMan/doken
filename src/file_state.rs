2b07a41ff1fde7f1a087093373e5776aa43da73d
Artur Ptaszek <artur@ptaszek.dev>
2024-03-10 13:19:28 +0100

    feat: Add failing test case
    Closes: #168

--- src/file_state.rs
+++ src/file_state.rs
@@ -105,9 +105,10 @@ impl FileState {
 
 #[cfg(test)]
 mod tests {
-    use std::time::SystemTime;
+    use std::time::{Duration, SystemTime};
 
     use tempfile::TempDir;
+    use tokio::time::sleep;
 
     use super::*;
 
@@ -372,15 +373,12 @@ mod tests {
                     refresh_token: None,
                     expires: None,
                     scope: None,
-                }
+                },
             )
             .await
             .unwrap();
         file_state
-            .upsert_token_info(
-                CLIENT_ID.to_owned(),
-                expected_token_info.to_owned()
-            )
+            .upsert_token_info(CLIENT_ID.to_owned(), expected_token_info.to_owned())
             .await
             .unwrap();
 
@@ -389,9 +387,64 @@ mod tests {
             .await
             .unwrap();
 
-        assert_eq!(actual_token_info.access_token, expected_token_info.access_token);
-        assert_eq!(actual_token_info.refresh_token, expected_token_info.refresh_token);
+        assert_eq!(
+            actual_token_info.access_token,
+            expected_token_info.access_token
+        );
+        assert_eq!(
+            actual_token_info.refresh_token,
+            expected_token_info.refresh_token
+        );
         assert_eq!(actual_token_info.expires, expected_token_info.expires);
         assert_eq!(actual_token_info.scope, expected_token_info.scope);
+    }
+
+    #[tokio::test]
+    async fn it_locks_rw_access_to_file() {
+        let (_tmp_dir, tmp_path) = get_tmp_path().unwrap();
+        const CLIENT_ID: &str = "test-client-id";
+        let expected_token_info = TokenInfo {
+            access_token: "expected_token_info".to_owned(),
+            refresh_token: None,
+            expires: None,
+            scope: None,
+        };
+
+        let client_id = CLIENT_ID.to_owned();
+        let tmp_path_1 = tmp_path.to_owned();
+        let handle = tokio::spawn(async move {
+            let file_state = FileState::from(tmp_path_1.to_owned());
+            sleep(Duration::from_millis(10)).await;
+            file_state
+                .upsert_token_info(
+                    client_id,
+                    TokenInfo {
+                        access_token: "not-important".to_owned(),
+                        refresh_token: None,
+                        expires: None,
+                        scope: None,
+                    },
+                )
+                .await
+                .unwrap();
+        });
+
+        let file_state = FileState::from(tmp_path.to_owned());
+        file_state
+            .upsert_token_info(CLIENT_ID.to_owned(), expected_token_info.to_owned())
+            .await
+            .unwrap();
+
+        let _ = handle.await;
+
+        let actual_token_info = file_state
+            .read_token_info(&CLIENT_ID.to_owned())
+            .await
+            .unwrap();
+
+        assert_eq!(
+            actual_token_info.access_token,
+            expected_token_info.access_token
+        );
     }
 }
