348e9af141aa75e427882817fca4cd71939e7217
Artur Ptaszek <artur.ptaszek@gmail.com>
2024-10-07 12:06:35 +0200

    fixup! fix: Issues with the latest Chrome

--- src/auth_browser/page.rs
+++ src/auth_browser/page.rs
@@ -165,7 +165,15 @@ impl Page {
             callback_url,
             move |event| match event.request.method.as_str() {
                 "POST" => {
-                    let body = event.request.post_data_entries.as_ref().unwrap().iter().map(|s| s.bytes.as_ref().unwrap().as_ref()).collect::<Vec<&[u8]>>().join("&".as_bytes());
+                    let body = event
+                        .request
+                        .post_data_entries
+                        .as_ref()
+                        .unwrap()
+                        .iter()
+                        .map(|s| s.bytes.as_ref().unwrap().as_ref())
+                        .collect::<Vec<&[u8]>>()
+                        .join("&".as_bytes());
 
                     log::info!("This is what we get in POST: {:?}", body);
                     let form_params =
